479491acafe91d3bcb97bfa8d585b7d4d14002ee
unity <unity@E423-019.loreto.internal>
2017-12-05 10:55:25 +0000

    Signed-off-by: unity <unity@E423-019.loreto.internal>

--- topDownShooter/Assets/Scripts/PlayerController.cs
+++ topDownShooter/Assets/Scripts/PlayerController.cs
@@ -13,9 +13,13 @@ public class PlayerController : MonoBehaviour {
 	public Transform bulletSpawn;
 	//Handling
 	public float rotationSpeed = 450f;
-	public float speed = 5f;
+	private float speed = 5f;
 	public float bulletVelocity = 6f;
 	public float bulletDuration = 2f;
+    public float BackwardsVelocity = 2f;
+    public float ForwardsVelocity = 6f;
+    public float SidewaysVelocity = 4f;
+    
 	//System Variables
 	private Quaternion targetRotation; private CharacterController controller;
 
@@ -47,8 +51,23 @@ public class PlayerController : MonoBehaviour {
 
 			
 		Vector3 motion = movement.normalized;
-		controller.Move(motion * Time.deltaTime * speed);
 
+        //Debug.Log(Vector3.Dot( transform.forward, motion));
+        //Debug.Log(motion);
+        if (Vector3.Dot(transform.forward, motion) > .4)
+        {
+            speed = ForwardsVelocity;
+        }
+        else if(Vector3.Dot(transform.forward, motion) < -.6)
+        {
+            speed = BackwardsVelocity;
+        }
+        else
+        {
+            speed = SidewaysVelocity;
+        }
+        controller.Move(motion * Time.deltaTime * speed);
+        Debug.Log(speed);
 		if (Input.GetKeyDown (KeyCode.Mouse0)) {
 			Fire ();
 		}
