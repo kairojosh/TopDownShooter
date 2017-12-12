c61712c91c3e200eb23d1e62cf4721f2e6f1ac16
unity <unity@E423-019.loreto.internal>
2017-12-12 11:13:14 +0000

    Done the reload time with sprite, seems out of place but when others are added will look fine
    Signed-off-by: unity <unity@E423-019.loreto.internal>

--- topDownShooter/Assets/Scripts/PlayerController.cs
+++ topDownShooter/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 [RequireComponent (typeof (CharacterController))]
@@ -8,10 +9,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	//References
+	//Reference
+
+	public RawImage CoolDown;
 	public GameObject bulletPrefab;
 	public Transform bulletSpawn;
 	//Handling
+	public float ReloadSpeed = 1f;
 	public float rotationSpeed = 450f;
 	private float speed = 5f;
 	public float bulletVelocity = 6f;
@@ -19,6 +23,7 @@ public class PlayerController : MonoBehaviour {
     public float BackwardsVelocity = 2f;
     public float ForwardsVelocity = 6f;
     public float SidewaysVelocity = 4f;
+	private float TimeStamp;
     
 	//System Variables
 	private Quaternion targetRotation; private CharacterController controller;
@@ -32,24 +37,23 @@ public class PlayerController : MonoBehaviour {
 			bullet.GetComponent<Rigidbody> ().velocity = bulletSpawn.transform.forward * bulletVelocity;
 
 		Destroy (bullet, bulletDuration);
+		CoolDown.texture = Resources.Load ("10_Empty") as Texture;
+		 TimeStamp = Time.time + ReloadSpeed;
 	}
 	// Use this for initialization
 	void Start () {
 
- controller = GetComponent<CharacterController>();	}
-	
+		 
+
+		controller = GetComponent<CharacterController>();TimeStamp = Time.time;	}
+
 
 	void FixedUpdate () {
 		Vector3 movement = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
-
-
-
 		Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - (transform.position);
 		difference.Normalize ();
 		targetRotation = Quaternion.LookRotation (difference);
 		transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle (transform.eulerAngles.y, targetRotation.eulerAngles.y, rotationSpeed * Time.deltaTime);
-
-			
 		Vector3 motion = movement.normalized;
 
         //Debug.Log(Vector3.Dot( transform.forward, motion));
@@ -67,9 +71,56 @@ public class PlayerController : MonoBehaviour {
             speed = SidewaysVelocity;
         }
         controller.Move(motion * Time.deltaTime * speed);
-        Debug.Log(speed);
-		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+        
+		//Debug.Log(speed);
+		Debug.Log (-(TimeStamp - Time.time)/ReloadSpeed);
+
+		if ((Input.GetKeyDown (KeyCode.Mouse0)) && (Time.time >= TimeStamp)) {
 			Fire ();
 		}
+
+
+		float TimeCalc = (TimeStamp - Time.time) * ReloadSpeed;
+
+			
+
+
+
+
+		if ((TimeCalc) > 0.9) {
+			CoolDown.texture = Resources.Load ("10_Empty") as Texture;
+		}
+		else if((TimeCalc ) >0.8) {
+
+			CoolDown.texture = Resources.Load ("9") as Texture;
+		}
+		else if((TimeCalc) > 0.7) {
+			CoolDown.texture = Resources.Load ("8") as Texture;
+		}
+		else if((TimeCalc) > 0.6) {
+			CoolDown.texture = Resources.Load ("7") as Texture;
+		}
+		else if((TimeCalc) > 0.5) {
+			CoolDown.texture = Resources.Load ("6") as Texture;
+		}
+		else if((TimeCalc) > 0.4) {
+			CoolDown.texture = Resources.Load ("5") as Texture;
+		}
+		else if((TimeCalc) > 0.3) {
+			CoolDown.texture = Resources.Load ("4") as Texture;
+		}
+		else if((TimeCalc) > 0.2 ) {
+			CoolDown.texture = Resources.Load ("3") as Texture;
+		}
+		else if((TimeCalc) > 0.1) {
+			CoolDown.texture = Resources.Load ("2") as Texture;
+		}
+		else {
+			CoolDown.texture = Resources.Load ("1_Full") as Texture;
+		}
+	
+		
+
+
 	}
 }
