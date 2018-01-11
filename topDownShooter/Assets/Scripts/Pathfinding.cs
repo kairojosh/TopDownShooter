1eed2c60d15168058fde8c5037049cae73c68f4d
Josh PC <joshpowell32@gmail.com>
2018-01-11 13:29:25 +0000

    NewNode
    etc.

--- topDownShooter/Assets/Scripts/Pathfinding.cs
+++ topDownShooter/Assets/Scripts/Pathfinding.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Pathfinding : MonoBehaviour {
+	GridScript grid;
 
-	// Use this for initialization
-	void Start () {
-		
+
+	void Awake(){
+		grid = GetComponent<GridScript> ();
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+	void FindPath(Vector3 startPosition, Vector3 targetPosition) {
+		Node startNode = grid.PositionConvertNode (startPosition);
+		Node targetNode = grid.PositionConvertNode (targetPosition);
+
+		List<Node> openNodes = new List<Node> ();
+		List<Node> closedNodes = new List<Node> ();
+
+		openNodes.Add (startNode);
+		while (closedNodes > 0) {
+			Node activeNode
+
+
+		}
 }
